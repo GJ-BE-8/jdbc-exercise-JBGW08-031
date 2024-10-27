8f1ed5d4092035c85eeb926c2563596b17bb521a
kyubin <kyubinlee22@gmail.com>
2024-10-27 17:21:18 +0900

    commit 2

--- src/test/java/com/nhnacademy/jdbc/util/BasicConnectionPoolTest.java
+++ src/test/java/com/nhnacademy/jdbc/util/BasicConnectionPoolTest.java
@@ -1,12 +1,16 @@
 package com.nhnacademy.jdbc.util;
 
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.params.ParameterizedTest;
-
 import java.sql.Connection;
 import java.sql.SQLException;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 @TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 class BasicConnectionPoolTest {
@@ -21,7 +25,8 @@ class BasicConnectionPoolTest {
     @BeforeAll
     static void setUp() {
         //todo#0 - jdbcUrl, username, password를 설정하세요
-        basicConnectionPool = new BasicConnectionPool(com.mysql.cj.jdbc.Driver.class.getName(),"","","",5);
+        basicConnectionPool = new BasicConnectionPool(com.mysql.cj.jdbc.Driver.class.getName(),
+                "jdbc:mysql://133.186.241.167:3306/nhn_academy_31", "nhn_academy_31", "3u.SF)xeXWIfzB[Y", 5);
     }
 
     @AfterAll
@@ -32,9 +37,9 @@ class BasicConnectionPoolTest {
     @Test
     @Order(0)
     @DisplayName("Driver not found Exception")
-    void init(){
+    void init() {
         Assertions.assertThrows(RuntimeException.class,
-            ()-> new BasicConnectionPool("org.mariadb.jdbc.Driver","jdbcUrl","userName","password",5)
+                () -> new BasicConnectionPool("org.mariadb.jdbc.Driver", "jdbcUrl", "userName", "password", 5)
         );
     }
 
@@ -46,10 +51,10 @@ class BasicConnectionPoolTest {
         connection2 = basicConnectionPool.getConnection();
         connection3 = basicConnectionPool.getConnection();
         Assertions.assertAll(
-                ()->Assertions.assertTrue(connection1.isValid(1000)),
-                ()->Assertions.assertTrue(connection2.isValid(1000)),
-                ()->Assertions.assertTrue(connection3.isValid(1000)),
-                ()->Assertions.assertEquals(basicConnectionPool.getUsedConnectionSize(),3)
+                () -> Assertions.assertTrue(connection1.isValid(1000)),
+                () -> Assertions.assertTrue(connection2.isValid(1000)),
+                () -> Assertions.assertTrue(connection3.isValid(1000)),
+                () -> Assertions.assertEquals(basicConnectionPool.getUsedConnectionSize(), 3)
         );
     }
 
@@ -58,12 +63,13 @@ class BasicConnectionPoolTest {
     @Order(2)
     @DisplayName("empty connection-pool")
     void getConnection_empty() throws InterruptedException, SQLException {
+
         connection4 = basicConnectionPool.getConnection();
         connection5 = basicConnectionPool.getConnection();
         Connection connection6 = basicConnectionPool.getConnection();
 
         Assertions.assertAll(
-                ()->Assertions.assertEquals(basicConnectionPool.getUsedConnectionSize(),5)
+                () -> Assertions.assertEquals(basicConnectionPool.getUsedConnectionSize(), 5)
         );
 
     }
@@ -76,6 +82,6 @@ class BasicConnectionPoolTest {
         basicConnectionPool.releaseConnection(connection2);
         basicConnectionPool.releaseConnection(connection3);
 
-        Assertions.assertEquals(basicConnectionPool.getUsedConnectionSize(),0);
+        Assertions.assertEquals(basicConnectionPool.getUsedConnectionSize(), 0);
     }
 }

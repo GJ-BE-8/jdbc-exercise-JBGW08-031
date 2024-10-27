f58189f5d008f343baa8ec25bb4156852e948042
kyubin <kyubinlee22@gmail.com>
2024-10-27 09:28:40 +0900

    commit 1

--- src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
+++ src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbUtils {
-    public DbUtils(){
+    public DbUtils() {
         throw new IllegalStateException("Utility class");
     }
 
@@ -13,7 +13,9 @@ public class DbUtils {
         Connection connection = null;
         try {
             //todo connection.
-            connection = DriverManager.getConnection("jdbc:mysql://133.186.241.167:3306/nhn_academy_0","nhn_academy_0","1j0OTaJgkS1@$dWl");
+            connection =
+                    DriverManager.getConnection("jdbc:mysql://133.186.241.167:3306/nhn_academy_31", "nhn_academy_31",
+                            "3u.SF)xeXWIfzB[Y");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }

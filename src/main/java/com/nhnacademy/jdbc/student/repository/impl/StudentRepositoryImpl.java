ae225e20b63659f287a90826ef285d1a6d6c582e
kyubin <kyubinlee22@gmail.com>
2024-10-28 10:19:07 +0900

    commit 1

--- src/main/java/com/nhnacademy/jdbc/student/repository/impl/StudentRepositoryImpl.java
+++ src/main/java/com/nhnacademy/jdbc/student/repository/impl/StudentRepositoryImpl.java
@@ -3,30 +3,33 @@ package com.nhnacademy.jdbc.student.repository.impl;
 import com.nhnacademy.jdbc.common.Page;
 import com.nhnacademy.jdbc.student.domain.Student;
 import com.nhnacademy.jdbc.student.repository.StudentRepository;
-import lombok.extern.slf4j.Slf4j;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class StudentRepositoryImpl implements StudentRepository {
 
     @Override
-    public int save(Connection connection, Student student){
+    public int save(Connection connection, Student student) {
         String sql = "insert into jdbc_students(id,name,gender,age) values(?,?,?,?)";
 
-        try(
-            PreparedStatement statement = connection.prepareStatement(sql);
-        ){
+        try (
+                PreparedStatement statement = connection.prepareStatement(sql);
+        ) {
             statement.setString(1, student.getId());
             statement.setString(2, student.getName());
             statement.setString(3, student.getGender().toString());
-            statement.setInt(4,student.getAge());
+            statement.setInt(4, student.getAge());
 
             int result = statement.executeUpdate();
-            log.debug("save:{}",result);
+            log.debug("save:{}", result);
             return result;
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -35,18 +38,18 @@ public class StudentRepositoryImpl implements StudentRepository {
     }
 
     @Override
-    public Optional<Student> findById(Connection connection,String id){
+    public Optional<Student> findById(Connection connection, String id) {
         String sql = "select * from jdbc_students where id=?";
-        log.debug("findById:{}",sql);
+        log.debug("findById:{}", sql);
 
         ResultSet rs = null;
-        try(
-            PreparedStatement statement = connection.prepareStatement(sql);
+        try (
+                PreparedStatement statement = connection.prepareStatement(sql);
         ) {
-            statement.setString(1,id);
+            statement.setString(1, id);
             rs = statement.executeQuery();
-            if(rs.next()){
-                Student student =  new Student(rs.getString("id"),
+            if (rs.next()) {
+                Student student = new Student(rs.getString("id"),
                         rs.getString("name"),
                         Student.GENDER.valueOf(rs.getString("gender")),
                         rs.getInt("age"),
@@ -57,7 +60,7 @@ public class StudentRepositoryImpl implements StudentRepository {
         } catch (SQLException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
-        }finally {
+        } finally {
             try {
                 rs.close();
             } catch (SQLException e) {
@@ -68,21 +71,21 @@ public class StudentRepositoryImpl implements StudentRepository {
     }
 
     @Override
-    public int update(Connection connection,Student student){
+    public int update(Connection connection, Student student) {
         String sql = "update jdbc_students set name=?, gender=?, age=? where id=?";
-        log.debug("update:{}",sql);
+        log.debug("update:{}", sql);
 
-        try(
-            PreparedStatement statement = connection.prepareStatement(sql);
+        try (
+                PreparedStatement statement = connection.prepareStatement(sql);
         ) {
-            int index=0;
+            int index = 0;
             statement.setString(++index, student.getName());
             statement.setString(++index, student.getGender().toString());
             statement.setInt(++index, student.getAge());
             statement.setString(++index, student.getId());
 
             int result = statement.executeUpdate();
-            log.debug("result:{}",result);
+            log.debug("result:{}", result);
             return result;
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -90,15 +93,15 @@ public class StudentRepositoryImpl implements StudentRepository {
     }
 
     @Override
-    public int deleteById(Connection connection,String id){
+    public int deleteById(Connection connection, String id) {
         String sql = "delete from jdbc_students where id=?";
 
-        try(
-            PreparedStatement statement = connection.prepareStatement(sql);
+        try (
+                PreparedStatement statement = connection.prepareStatement(sql);
         ) {
             statement.setString(1, id);
             int result = statement.executeUpdate();
-            log.debug("result:{}",result);
+            log.debug("result:{}", result);
             return result;
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -109,11 +112,11 @@ public class StudentRepositoryImpl implements StudentRepository {
     public int deleteAll(Connection connection) {
         String sql = "delete from jdbc_students";
 
-        try(
+        try (
                 PreparedStatement statement = connection.prepareStatement(sql);
         ) {
             int result = statement.executeUpdate();
-            log.debug("result:{}",result);
+            log.debug("result:{}", result);
             return result;
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -123,13 +126,74 @@ public class StudentRepositoryImpl implements StudentRepository {
     @Override
     public long totalCount(Connection connection) {
         //todo#4 totalCount 구현
+        String sql = "select count(*) from jdbc_students";
+        ResultSet rs = null;
+
+        try (PreparedStatement psmt = connection.prepareStatement(sql)) {
+            rs = psmt.executeQuery();
+            if (rs.next()) {
+                return rs.getLong(1);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (Objects.nonNull(rs)) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
         return 0l;
     }
 
     @Override
     public Page<Student> findAll(Connection connection, int page, int pageSize) {
         //todo#5 페이징 처리 구현
-        return null;
+        int offset = (page - 1) * pageSize;
+        int limit = pageSize;
+
+        ResultSet rs = null;
+        String sql = "select id, name, gender, age, created_at from jdbc_students order by id desc limit  ?,? ";
+        try (PreparedStatement psmt = connection.prepareStatement(sql)) {
+            psmt.setInt(1, offset);
+            psmt.setInt(2, limit);
+            rs = psmt.executeQuery();
+            List<Student> studentList = new ArrayList<>(pageSize);
+
+            while (rs.next()) {
+                studentList.add(
+                        new Student(
+                                rs.getString("id"),
+                                rs.getString("name"),
+                                Student.GENDER.valueOf(rs.getString("gender")),
+                                rs.getInt("age"),
+                                rs.getTimestamp("created_at").toLocalDateTime()
+                        )
+                );
+            }
+
+            long total = 0;
+
+            if (!studentList.isEmpty()) {
+                // size>0 조회 시도, 0이면 조회할 필요 없음, count query는 자원을 많이 소모하는 작업
+                total = totalCount(connection);
+            }
+
+            return new Page<Student>(studentList, total);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (Objects.nonNull(rs)) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
 }
--- src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
+++ src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
@@ -1,11 +1,11 @@
 package com.nhnacademy.jdbc.util;
 
-import org.apache.commons.dbcp2.BasicDataSource;
-import javax.sql.DataSource;
 import java.time.Duration;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 public class DbUtils {
-    public DbUtils(){
+    public DbUtils() {
         throw new IllegalStateException("Utility class");
     }
 
@@ -15,9 +15,9 @@ public class DbUtils {
         BasicDataSource basicDataSource = new BasicDataSource();
 
         //todo#0 {ip},{database},{username},{password}
-        basicDataSource.setUrl("jdbc:mysql://{ip}:3306/{database}");
-        basicDataSource.setUsername("{username}");
-        basicDataSource.setPassword("{password}");
+        basicDataSource.setUrl("jdbc:mysql://133.186.241.167:3306/nhn_academy_31");
+        basicDataSource.setUsername("nhn_academy_31");
+        basicDataSource.setPassword("3u.SF)xeXWIfzB[Y");
 
         basicDataSource.setInitialSize(5);
         basicDataSource.setMaxTotal(5);
@@ -30,7 +30,7 @@ public class DbUtils {
         DATASOURCE = basicDataSource;
     }
 
-    public static DataSource getDataSource(){
+    public static DataSource getDataSource() {
         return DATASOURCE;
     }
 
